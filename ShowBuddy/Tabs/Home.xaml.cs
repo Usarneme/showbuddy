84116b6d9c7105c47a9848e05c4b2bcd3767ec43
Usarneme <jamestcoop@gmail.com>
2024-03-04 21:20:07 -0800

    :art: updates layout of home page items

--- ShowBuddy/Tabs/Home.xaml.cs
+++ ShowBuddy/Tabs/Home.xaml.cs
@@ -2,20 +2,8 @@
 
 public partial class Home : ContentPage
 {
-	int count = 0;
-
 	public Home()
 	{
 		InitializeComponent();
 	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-    CounterBtn.Text = $"Clicked {count} time";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
 }
-
--- ShowBuddy/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/ShowBuddy.AssemblyInfo.cs
+++ ShowBuddy/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/ShowBuddy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShowBuddy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+da7c4151ff4d740cc2b24cae211f282adf299efd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a823bdbd58b1390fff23e4cc0b6a9317768999b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShowBuddy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShowBuddy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
